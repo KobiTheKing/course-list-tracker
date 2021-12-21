e30e88935d2c745d000eff299fc1b65002f6afe4
Kobi Feldman <kobif2001@gmail.com>
2021-12-21 16:56:05 -0500

    Update messageHandler.py Moved send message functionality into a function.

--- messageHandler.py
+++ messageHandler.py
@@ -2,17 +2,20 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 import os
 
+#Puts the environment variables from .env into os.environ
 load_dotenv()
 
-#TODO change to environment variables
 accountSID = os.environ.get('ACCOUNT_SID')
 authToken = os.environ.get('AUTH_TOKEN')
 client = Client(accountSID, authToken)
 
-#Create test message
-msgString = 'This is a test message!'
-message = client.messages.create(
-    to = os.environ.get('PERSONAL_PHONE'),
-    from_ = os.environ.get('TWILIO_PHONE'),
-    body = msgString
-)
+#Sends a test message
+def sendMessage(msgString):
+    message = client.messages.create(
+        to = os.environ.get('PERSONAL_PHONE'),
+        from_ = os.environ.get('TWILIO_PHONE'),
+        body = msgString
+    )
+
+msgString = 'Hello!'
+sendMessage(msgString)
