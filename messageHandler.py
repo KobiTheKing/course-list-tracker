d38283a9e4aebb1a75627886dd36fc7d39820cc0
Kobi Feldman <kobif2001@gmail.com>
2021-12-21 16:20:49 -0500

    Update messageHandler.py Wrote simple test message.

--- messageHandler.py
+++ messageHandler.py
@@ -1,5 +1,18 @@
 from twilio.rest import Client
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 #TODO change to environment variables
-accountSID = ''
-authToken = ''
+accountSID = os.environ.get('ACCOUNT_SID')
+authToken = os.environ.get('AUTH_TOKEN')
+client = Client(accountSID, authToken)
+
+#Create test message
+msgString = 'This is a test message!'
+message = client.messages.create(
+    to = os.environ.get('PERSONAL_PHONE'),
+    from_ = os.environ.get('TWILIO_PHONE'),
+    body = msgString
+)
