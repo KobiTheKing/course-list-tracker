48a3c252faa18eb869b4aaa7bf51517678785d81
Kobi Feldman <kobif2001@gmail.com>
2021-12-22 17:20:15 -0500

    docs: update scraper.py
    Added some comments to createURL(subject)

--- scraper.py
+++ scraper.py
@@ -1,8 +1,12 @@
 from bs4 import BeautifulSoup
 import requests
 
+# A basic url for the WM Open Course List that can be added too
 BASE_URL = "https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202220&term_subj=&attr=0&attr2=0&levl=0&status=0&ptrm=0&search=Search"
 
+
+# Creates a url for a subject page on the WM Open Course List
+# param subject: the subject identifier to be inserted into the URL (Examples: CSCI, BIOL, THEA, etc)
 def createURL(subject):
     insertLocStr = "term_subj="
     insertLocIndex = BASE_URL.find(insertLocStr)
