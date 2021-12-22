441fad325ec1135a0a88813353ffe17088b05de8
Kobi Feldman <kobif2001@gmail.com>
2021-12-22 18:23:20 -0500

    Update scraper.py
    Wrote scrape(URL) function.

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
+from requests.exceptions import RequestException
 
 # A basic url for the WM Open Course List that can be added too
 BASE_URL = "https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202220&term_subj=&attr=0&attr2=0&levl=0&status=0&ptrm=0&search=Search"
@@ -24,10 +25,25 @@ def createURL(subject):
 
     return BASE_URL[:insertLocIndex + 10] + subject + BASE_URL[insertLocIndex + 10:]
 
-# Attempts to scrape a given URL.
+# Attempts to scrape a web page.
+# param URL: the url to scrape
 # return: the html code of the web page
-# throw: exception if the URL doesn't exist or the request times out
+# raise: exception if the URL doesn't exist or the request times out
 def scrape(URL):
-    pass
+    try:
+        source = requests.get(URL, timeout = 10.000)
+    except requests.exceptions.RequestException as e:
+        raise RequestException
 
-print(createURL("BIOL"))
+    soup = BeautifulSoup(source.text, "lxml")
+
+    # Checks if the url leads to a special error page
+    if soup.find("h1", class_ = "bannerTitle").a.text.strip() == "Error":
+        raise RequestException
+
+    return soup
+
+try:
+    print(scrape(createURL("CSCI")))
+except requests.exceptions.RequestException as e:
+    print("AHHH! An error")
