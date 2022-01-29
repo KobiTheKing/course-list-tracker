f30d9a279eeb110fe4eb4a32fbc8c648bc76ef9e
Kobi Feldman <kobif2001@gmail.com>
2022-01-29 00:37:30 -0500

    Update bot.py
    Added some basic logging.

--- hikari_lightbulb_bot/bot.py
+++ hikari_lightbulb_bot/bot.py
@@ -2,17 +2,23 @@ import os
 import dotenv
 import hikari
 import lightbulb
+import logging
 
 dotenv.load_dotenv()
 
+# Creates a bot instance
 bot = lightbulb.BotApp(
     os.environ["DISCORD_BOT_TOKEN"],
     intents = hikari.Intents.ALL,
-    default_enabled_guilds=(833429143672717315,)
+    default_enabled_guilds = (833429143672717315),
+    logs = "INFO"
 )
 
+# Register command to bot
 @bot.command
+# Convert function into command
 @lightbulb.command("ping", description="The bot's ping")
+# Define command's type
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx: lightbulb.Context) -> None:
     await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
