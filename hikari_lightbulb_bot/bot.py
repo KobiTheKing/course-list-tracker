c2e0d749121d650a6732f2662d2a57fc984513e9
Kobi Feldman <kobif2001@gmail.com>
2022-02-09 22:02:38 -0500

    Add starttracking.py

--- hikari_lightbulb_bot/bot.py
+++ hikari_lightbulb_bot/bot.py
@@ -17,12 +17,24 @@ bot = lightbulb.BotApp(
 # Register command to bot
 @bot.command
 # Convert function into command
-@lightbulb.command("ping", description="The bot's ping")
+@lightbulb.command("ping", description = "The bot's ping")
 # Define command's type
 @lightbulb.implements(lightbulb.SlashCommand)
 async def ping(ctx: lightbulb.Context) -> None:
-    test(ctx)
+    #test(ctx)
     await ctx.respond(f"Pong! Latency: {bot.heartbeat_latency*1000:.2f}ms")
+
+# Command to clear all the messages in a channel
+@bot.command
+@lightbulb.command("clear", description = "Clears the message history in the current channel.")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def clear(ctx: lightbulb.Context) -> None:
+    channel = ctx.get_channel()
+
+    if channel.name == "bot-commands" and channel.type == hikari.ChannelType.GUILD_TEXT:
+        hikari.GuildTextChannel(channel).delete_messages()
+
+    await ctx.respond("Channel contents cleared.")
 
 def test(ctx: lightbulb.Context):
     print(ctx.app)
@@ -36,4 +48,5 @@ def test(ctx: lightbulb.Context):
 
 
 if __name__ == "__main__":
+    bot.load_extensions("commands.starttracking")
     bot.run()
---
+++ hikari_lightbulb_bot/commands/starttracking.py
@@ -0,0 +1,21 @@
+import lightbulb
+
+# Plugins are structures that allow the grouping of multiple commands and listeners together.
+plugin = lightbulb.Plugin("StartTrackingPlugin")
+
+# Creates a command in the plugin
+@plugin.command
+@lightbulb.command("starttracking", description = "TEMP DESCRIPTION")
+@lightbulb.implements(lightbulb.SlashCommand)
+async def startTracking(ctx: lightbulb.Context) -> None:
+    await ctx.user.send(content = "Test message!")
+    await ctx.respond("temp")
+
+
+# Extensions are hot-reloadable (can be loaded/unloaded while the bot is live)
+
+def load(bot):
+    bot.add_plugin(plugin)
+
+def unload(bot):
+    bot.remove_plugin(plugin)
