ed64d9b860202b02979b864b8a15038a92b3e567
Kobi Feldman <kobif2001@gmail.com>
2022-07-05 20:25:21 -0400

    Update scraper.py
    Change base url

--- scraper.py
+++ scraper.py
@@ -3,7 +3,7 @@ import requests
 from requests.exceptions import RequestException
 
 # A basic url for the WM Open Course List that can be added too
-BASE_URL = "https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202220&term_subj=&attr=0&attr2=0&levl=0&status=0&ptrm=0&search=Search"
+BASE_URL = "https://courselist.wm.edu/courselist/courseinfo/searchresults?term_code=202310&term_subj=&attr=0&attr2=0&levl=0&status=0&ptrm=0&search=Search"
 
 # Scrapes the page for the given subject, finds the row (in table) with the given CRN and returns its status (open OR closed).
 # param CRN: the unique identifier for the course in the given subject
